6017c8a0af650a8a9a83a06bf9458e11963d45b1
sanjaykumar <sanjay.m@dal.ca>
2019-11-21 19:36:49 -0400

    Added sign out, delete account, change password options in Profile page

--- app/src/main/java/com/dal/mc/servicegenie/MainActivity.java
+++ app/src/main/java/com/dal/mc/servicegenie/MainActivity.java
@@ -13,17 +13,11 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
     String[] values = {"Home Cleaning", "Plumber", "Electrician", "Painting", "Beauty & Salon", "Movers and Packers",
             "Pest Control", "Carpenter", "Party Planning"
     };
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseUser user = auth.getCurrentUser();
+        if (user == null) {
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
